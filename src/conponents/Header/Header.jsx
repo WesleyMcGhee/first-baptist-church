fd1eed1843fe3aba8c63f2cfc5dad90a4325c6a3
Wesley McGhee <wesleymcghee@MWI-wm-C02CV02EML7K.lan>
2022-02-27 12:52:48 -0600

    test

--- src/conponents/Header/Header.jsx
+++ src/conponents/Header/Header.jsx
@@ -1,14 +1,12 @@
 import "./Header.css";
 import logo from "../../photos/fbclogo.PNG";
 
-const mapThroughLinks = (arr) => {
-  arr.map((key) => {});
-};
-
-const Header = (props) => {
+const Header = () => {
   return (
     <header>
       <img src={logo} />
     </header>
   );
 };
+
+export default Header;
