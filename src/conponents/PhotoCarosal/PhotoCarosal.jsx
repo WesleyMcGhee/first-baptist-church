242f1a0b52f367508b37c7d18a1eb6a70f56b3c8
Wesley McGhee <wesleymcghee@MWI-wm-C02CV02EML7K.lan>
2022-03-05 09:54:20 -0600

    wip

--- src/conponents/PhotoCarosal/PhotoCarosal.jsx
+++ src/conponents/PhotoCarosal/PhotoCarosal.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
 import "./PhotoCarosal.css";
 
 const PhotoCarosal = (props) => {
+  const arr = props.arr;
+  const height = props.height;
+  const width = props.width;
+  let currentIndex = 0;
+
+  const changePhoto = () => {
+    setInterval(() => {
+      if (currentIndex === arr.length) {
+        return arr[0];
+      }
+    });
+  };
   return <h1>Hello World</h1>;
 };
